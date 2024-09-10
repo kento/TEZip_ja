8c1b23b8d2498f260e37473e55bf6e7256fe91d8
amarjitkalsy <117324017+amarjitkalsy@users.noreply.github.com>
2024-09-10 15:20:12 +0900

    Update conf.py
    Sphinx configurations

--- docs/conf.py
+++ docs/conf.py
@@ -14,7 +14,7 @@
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 
-
+latex_engine = "platex"
 # -- Project information -----------------------------------------------------
 
 project = 'TEZip'
